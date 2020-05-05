9f91db75dd8ad59f200f265282a211b8119cf50a
markjamesyork <markjamesyork@gmail.com>
2020-05-05 10:24:49 -0500

    SGLD

--- src/Stochastic_Gradient_Langevin_Dynamics/model.py
+++ src/Stochastic_Gradient_Langevin_Dynamics/model.py
@@ -1,5 +1,5 @@
 from src.base_net import *
-from optimizers import *
+from src.Stochastic_Gradient_Langevin_Dynamics.optimizers import *
 import torch.nn as nn
 import torch.nn.functional as F
 import copy
